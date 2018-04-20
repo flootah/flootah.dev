23a23ef68ab50b3dcd816b3f7fe1b56e1b65ea76
Eduardo <esaenziusd@gmail.com>
2018-04-19 21:37:09 -0700

    wakaba engine updates
    more more more more

--- projects/wakaba/lib/wakaba.js
+++ projects/wakaba/lib/wakaba.js
@@ -1,13 +1,19 @@
+//TODO sometimes things dont line up, and the shit activates twice.
 
-
+//
 const transitions = [0, 0.968, 1.368, 1.969, 2.903, 3.804, 4.471, 5.005, 6.540];
+//points of seek in which the video should change in state.
 var change = [1.4, 1.9, 3.3, 3.7, 5.1, 5.6, 7.5,  8.9, 9.4, 10.8, 11.2, 12.6, 13.1, 15.0,  16.4, 16.9, 18.3,  18.7, 20.1, 20.6];
+//video element
 var vid = document.getElementById("wakaba");
+//current video state
 var state = 0;
+//how far in the music is.
 var seek;
+//set video to be muted and to loop.
 vid.muted = true;
 vid.loop = true;
-
+//not sure what's going on here... but I know both of these variable have to exist. I wont mess with this yet.
 var sound = new Howl({
     src: ['src/audio/doublejump.mp3', 'src/audio/doublejump.ogg'],
     loop: true,
@@ -16,7 +22,8 @@ var sound = new Howl({
     onend: nextState,
   } );
 var doublejump = sound;
-
+//true if currently transitioning, false otherwise.
+var trans = false;
 /**
  * loops state from 1-8
  */
@@ -27,7 +34,7 @@ function nextState() {
         state = 1;
     }
     transition();
-    //console.log(state);
+    console.log(state);
 };
 
 /**
@@ -50,14 +57,24 @@ function pauseVid(currentstate) {
         if(currentstate == state) vid.pause();
 };
 
+/**
+ * makes the video transition to the next shot.
+ * 
+ * based on value of state, it will:
+ * -seek the video to a predefined point, based on state.
+ * -play the video.
+ * -after 600ms (500 in one case) it pauses the video.
+ * 
+ */
 function transition() {
-switch(state) {
-        case 0:
+    switch(state) {
+        case 0:s
         vid.play();
         setTimeout(function() {
             pauseVid(0);
         }, 600);
         break;
+
         case 1:
         vid.currentTime = transitions[1];
         vid.play();
@@ -65,6 +82,7 @@ switch(state) {
             pauseVid(1);
         }, 500);
         break;
+
         case 2:
         vid.currentTime = transitions[2];
         vid.play();
@@ -72,6 +90,7 @@ switch(state) {
             pauseVid(2);
         }, 600);
         break;
+
         case 3:
         vid.currentTime = transitions[3];
         vid.play();
@@ -79,6 +98,7 @@ switch(state) {
             pauseVid(3);
         }, 600);
         break;
+
         case 4:
         vid.currentTime = transitions[4];
         vid.play();
@@ -86,6 +106,7 @@ switch(state) {
             pauseVid(4);
         }, 600);
         break;
+
         case 5:
         vid.currentTime = transitions[5];
         vid.play();
@@ -93,6 +114,7 @@ switch(state) {
             pauseVid(5);
         }, 600);
         break;
+
         case 6:
         vid.currentTime = transitions[6];
         vid.play();
@@ -100,6 +122,7 @@ switch(state) {
             pauseVid(6);
         }, 600);
         break;
+
         case 7:
         vid.currentTime = transitions[7];
         vid.play();
@@ -107,6 +130,7 @@ switch(state) {
             pauseVid(7);
         }, 600);
         break; 
+
         case 8:
         vid.currentTime = transitions[8];
         vid.play();
@@ -114,6 +138,7 @@ switch(state) {
             pauseVid(8);
         }, 1000);
         break;
+
         default:
         console.log(state);
         console.log('yeah something is def wrong');
@@ -122,24 +147,37 @@ switch(state) {
 
 };
 
+/**
+ * the "main" function of the wakaba engine.
+ * starts the sound
+ * runs changeState (0 --> 1)
+ * begins statemachine()
+ */
 function begin() {
     console.log("begin() now running")
     sound.play();
-    transition();
+    nextState();
     statemachine();
 };
 
+/**
+ * after 1.25s, set value of seek, then start a .1s interval function.
+ * this interval function will:
+ * set the value of seek,
+ * compare with with any values within change,
+ * if there is a match, change state and log boop!
+ */
 function statemachine() {
-setTimeout(function() {
-    seek = parseFloat(sound.seek(doublejump).toFixed(1));
-    setInterval(function(){
-        seek = parseFloat(sound.seek(doublejump).toFixed(1)); 
-        if(change.includes(seek)) {
-            nextState();
-            console.log("boop!");
-        }
-    }, 100);
-}, 1250);
+    setTimeout(function() {
+        seek = parseFloat(sound.seek(doublejump).toFixed(1));
+        setInterval(function(){
+            seek = parseFloat(sound.seek(doublejump).toFixed(1)); 
+            if(change.includes(seek)) {
+                nextState();
+                console.log("boop!");
+            }
+        }, 100);
+    }, 1250);
 }
 
 // get infobox
