2a2cfe0c074608dd561a4ea9848a3fa0b83c63e8
flootah <ed.saenz@protonmail.ch>
2020-04-14 17:09:21 -0700

    wur update
    probably good

--- projects/make-things-better/script.js
+++ projects/make-things-better/script.js
@@ -1,9 +1,8 @@
+console.log("heya")
 var i = 0;
-let words = ["hi", "hello", "bonjour", "heya", "こんにちは"]
-
-function press () {
+let words = ["copy wur", "i read u", "are you well?", "things /should/ be made better", "message repeats."]
+function press() {
     console.log(words[i])
-
-    if (i == 4) i = -1
+    if (i == words.length-1) i = -1
     i++;
 }
