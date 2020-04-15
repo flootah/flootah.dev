cb428bf5822b033d84c27d54ca26cc7485caccbc
flootah <ed.saenz@protonmail.ch>
2020-04-14 17:12:52 -0700

    Update script.js

--- projects/make-things-better/script.js
+++ projects/make-things-better/script.js
@@ -1,6 +1,6 @@
 console.log("heya")
 var i = 0;
-let words = ["copy wur", "i read u", "are you well?", "things /should/ be made better", "message repeats."]
+var words = ["copy wur", "i read u", "are you well?", "things /should/ be made better", "message repeats."]
 function press() {
     console.log(words[i])
     if (i == words.length-1) i = -1
