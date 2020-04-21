9edb99cb5cc8e68d516bdf36c9529007189cac97
flootah <ed.saenz@protonmail.ch>
2020-04-21 12:04:31 -0700

    mmmh.

--- projects/make-things-better/script.js
+++ projects/make-things-better/script.js
@@ -1,8 +1,14 @@
 console.log("heya")
 var i = 0;
+var presses = 0;
 var words = ["copy wur", "i read u", "are you well?", "things /should/ be made better", "message repeats."]
 function press() {
     console.log(words[i])
     if (i == words.length-1) i = -1
-    i++;
+    i++
+    if(presses == 10) {
+        alert("it wont get better.")
+        return
+    }
+    presses++
 }
